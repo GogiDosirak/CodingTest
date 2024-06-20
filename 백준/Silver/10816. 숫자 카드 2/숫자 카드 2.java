475feb2301506a454d7e4814da4ed70baec24296
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-06-21 03:15:56 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 904 ms, Memory: 197552 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,50 +1,36 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int N = sc.nextInt();
-        sc.nextLine();
+        int N = Integer.parseInt(br.readLine());
 
-        int[] array1 = new int[10000001]; // 양수
-        int[] array2 = new int[10000001]; // 음수
+        int[] cnt = new int[20000001]; //-10000000~10000000
 
-        // 카운팅 정렬? -> 음수는 안됨 => 음수방향 카운팅도 하나 만들기
-        for (int i = 0; i < N; i++) {
-            int a = sc.nextInt();
-            if(a > 0) {
-                array1[a]++;
-            } else {
-                array2[Math.abs(a)]++; // - 떼서 ++
-            }
-        }
-        sc.nextLine();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-
-        int M = sc.nextInt();
-        sc.nextLine();
-        int[] array3 = new int[M];
-        for (int i = 0; i < M; i++) {
-            array3[i] = sc.nextInt();
+        for (int i = 0; i < N ; i++) {
+            cnt[Integer.parseInt(st.nextToken()) + 10000000]++;
         }
 
+        int M = Integer.parseInt(br.readLine());
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            if(array3[i] > 0) {
-                sb.append(array1[array3[i]]).append('\n');
-            } else {
-                sb.append(array2[Math.abs(array3[i])]).append(" ");
-            }
+            sb.append(cnt[Integer.parseInt(st2.nextToken())+10000000]).append(" ");
+
         }
         System.out.println(sb);
 
 
 
-        }
-
-
 
         }
 
+
+
+        }
 
