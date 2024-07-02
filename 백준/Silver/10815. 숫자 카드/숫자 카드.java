a12d3199cb9295b8cc3ca7bade956a84de79a788
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-07-02 16:53:39 +0900

    [Silver V] Title: 숫자 카드, Time: 700 ms, Memory: 176052 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] cnt = new int[20000001];
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            cnt[arr[i]+10000000]++;
+        }
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int b = Integer.parseInt(st.nextToken())+10000000;
+            sb.append(cnt[b]).append(" ");
+        }
+        System.out.println(sb);
+
+
+    }
+
+}
