e61bfab0085953bb265c3c48d3a0ac8f182e9739
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-08-17 04:00:04 +0900

    [Silver III] Title: N과 M (5), Time: 1708 ms, Memory: 69720 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N,M;
+    static int[] pm, arr, ch;
+    public void solve(int L, int index) {
+        if(L == M) {
+            for (int x : pm) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        } else {
+            for (int i = 0; i < N; i++) {
+                if(ch[i] == 0) {
+                    ch[i] = 1;
+                    pm[L] = arr[i];
+                    solve(L + 1, index);
+                    ch[i] = 0;
+                }
+            }
+        }
+
+
+    }
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        pm = new int[M];
+        arr = new int[N];
+        ch = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        T.solve(0,0);
+
+
+    }
+}
+
