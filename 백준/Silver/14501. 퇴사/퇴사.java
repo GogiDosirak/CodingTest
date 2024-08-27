d53d71b639137f098172f8624b79e128afe2a126
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-08-28 07:43:04 +0900

    [Silver III] Title: 퇴사, Time: 100 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -19,15 +19,14 @@ public class Main {
     static Work[] arr;
     public int dynamic(int N) {
         for (int i = 1; i <= N; i++) {
-            if(i+arr[i].time <= N+1) {
+            if(i+arr[i].time <= N+1) { // 날짜가 범위를 넘어가지 않는 경우
                 dp[i + arr[i].time] = Math.max(dp[i + arr[i].time], dp[i] + arr[i].price); // 현재값과 더해져서 만들어진 값 중에 더 최대값으로 갱신
-            } // 안되는 경우엔 전 배열 중 최대값? + 퇴사 당일날 끝나는 경우도 포함
+                // 왜? 10일이 마지막이고 1일을 소요하는 경우, 11일째의 금액을 구해야하기 때문
+            } // 안되는 경우엔 전날의 값을 넣어줌, 그러면 자연스레 퇴사일의 최대값도 들어간다.
             dp[i+1] = Math.max(dp[i+1], dp[i]);
-
         }
-        return dp[N+1];
-
-
+        return dp[N+1]; // 퇴사일(N+1)의 최대 수익을 반환
+        
     }
  public static void main(String[] args) throws IOException {
         Main M = new Main();
@@ -42,9 +41,6 @@ public class Main {
      }
      dp = new int[N+2]; // 일수가 지날수록 얼마나 얻을 수 있는지 최대수입, 퇴사 다음날에 상담이 완료될 수 있으므로 N+2
      System.out.println(M.dynamic(N));
-
-
-
     }
 }
 
