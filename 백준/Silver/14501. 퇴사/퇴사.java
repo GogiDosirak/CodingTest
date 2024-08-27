628fbeb542dd184a8a5b18d67c2cdf005eebea5f
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-08-28 07:25:23 +0900

    [Silver III] Title: 퇴사, Time: 100 ms, Memory: 14444 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+class Work {
+    int time;
+    int price;
+
+    public Work(int time, int price) {
+        this.time = time;
+        this.price = price;
+    }
+}
+public class Main {
+    static int N, T, P;
+    static int[] dp;
+    static Work[] arr;
+    public int dynamic(int N) {
+        for (int i = 1; i <= N; i++) {
+            if(i+arr[i].time <= N+1) {
+                dp[i + arr[i].time] = Math.max(dp[i + arr[i].time], dp[i] + arr[i].price); // 현재값과 더해져서 만들어진 값 중에 더 최대값으로 갱신
+            } // 안되는 경우엔 전 배열 중 최대값? + 퇴사 당일날 끝나는 경우도 포함
+            dp[i+1] = Math.max(dp[i+1], dp[i]);
+
+        }
+        return dp[N+1];
+
+
+    }
+ public static void main(String[] args) throws IOException {
+        Main M = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new Work[N+1];
+     for (int i = 1; i <= N; i++) {
+         StringTokenizer st = new StringTokenizer(br.readLine());
+         T = Integer.parseInt(st.nextToken());
+         P = Integer.parseInt(st.nextToken());
+         arr[i] = new Work(T,P);
+     }
+     dp = new int[N+2]; // 일수가 지날수록 얼마나 얻을 수 있는지 최대수입, 퇴사 다음날에 상담이 완료될 수 있으므로 N+2
+     System.out.println(M.dynamic(N));
+
+
+
+    }
+}
+
