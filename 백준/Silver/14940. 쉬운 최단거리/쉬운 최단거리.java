82dd9a2899c1073081fbde2ba4e8284121cef4ee
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-09-08 09:30:12 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 1684 ms, Memory: 73912 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -16,20 +16,21 @@ public class Main {
     static int M, N, px, py, level;
     static int[][] arr;
     static int[][] answer;
-    int[] dx = {1,0,-1,0};
-    int[] dy = {0,1,0,-1};
+    int[] dx = {1, 0, -1, 0};
+    int[] dy = {0, 1, 0, -1};
+
     public void BFS(int x, int y) {
         Queue<Point> Q = new LinkedList<>();
-        Q.offer(new Point(x,y));
-        while(!Q.isEmpty()) {
+        Q.offer(new Point(x, y));
+        while (!Q.isEmpty()) {
             int len = Q.size();
             for (int i = 0; i < len; i++) {
                 Point p = Q.poll();
                 for (int j = 0; j < 4; j++) {
                     int nx = p.x + dx[j];
                     int ny = p.y + dy[j];
-                    if(nx >= 0 && nx < N && ny >= 0 && ny < M && arr[nx][ny] == 1) {
-                        Q.offer(new Point(nx,ny));
+                    if (nx >= 0 && nx < N && ny >= 0 && ny < M && arr[nx][ny] == 1) {
+                        Q.offer(new Point(nx, ny));
                         arr[nx][ny] = 2;
                         answer[nx][ny] = level;
                     }
@@ -42,6 +43,7 @@ public class Main {
 
 
     }
+
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -54,32 +56,27 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
-                if(arr[i][j] == 2) {
+                if (arr[i][j] == 2) {
                     px = i;
                     py = j;
                 }
             }
         }
         level = 1;
-        T.BFS(px,py);
+        T.BFS(px, py);
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if(arr[i][j] == 1) {
+                if (arr[i][j] == 1) {
                     answer[i][j] = -1;
                 }
-            }
-        }
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
                 System.out.print(answer[i][j] + " ");
-                }
-            System.out.println();
             }
+            System.out.println();
         }
 
 
     }
+}
 
 
 
