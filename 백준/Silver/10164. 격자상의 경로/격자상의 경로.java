79b37c64a56976d53b5761c72d6448d3a1cb88d5
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-09-27 11:41:04 +0900

    [Silver II] Title: 격자상의 경로, Time: 100 ms, Memory: 14268 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10164. 격자상의 경로/격자상의 경로.java
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.java
@@ -1,42 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.util.*;
-
-
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N,M,K;
-    static int[][] arr;
-    static int[][] dp, ch;
-    static int ax,ay,cnt;
-    int[] dx = {0,1};
-    int[] dy = {1,0};
-    public void DFS(int x, int y) {
-        if(K != 0) {
-            if (ch[ax][ay] == 1 && x == N && y == M) {
-                cnt++;
-                return;
-            }
-        } else {
-            if (x == N && y == M) {
-                cnt++;
-                return;
-            }
+    static int N, M, K;
+    static int[][] dp1, dp2;
+    static int ax, ay;
 
-        }
-        for (int i = 0; i < 2; i++) {
-            int nx = x+dx[i];
-            int ny = y+dy[i];
-            if(nx>= 1 && nx <= N && ny>=1 && ny <= M && ch[nx][ny] == 0) {
-                ch[nx][ny] = 1;
-                DFS(nx,ny);
-                ch[nx][ny] = 0;
+    // 출발점에서 경유점까지의 경로 계산
+    public void dynamic1(int x, int y) {
+        for (int i = 1; i <= x; i++) {
+            for (int j = 1; j <= y; j++) {
+                if (i == 1 && j == 1) continue;
+                dp1[i][j] = (i > 1 ? dp1[i - 1][j] : 0) + (j > 1 ? dp1[i][j - 1] : 0);
             }
         }
-
     }
+
+    // 경유점에서 도착점까지의 경로 계산
+    public void dynamic2(int x, int y) {
+        for (int i = 1; i <= x; i++) {
+            for (int j = 1; j <= y; j++) {
+                if (i == 1 && j == 1) continue;
+                dp2[i][j] = (i > 1 ? dp2[i - 1][j] : 0) + (j > 1 ? dp2[i][j - 1] : 0);
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -44,20 +35,34 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        arr = new int[N+1][M+1];
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j < M; j++) {
-                arr[i][j] = (i-1)*5 + j;
-            }
+
+        // 경유점 K가 있을 때 좌표 계산
+        if (K != 0) {
+            ax = (K - 1) / M + 1;
+            ay = (K - 1) % M + 1;
+        } else {
+            ax = 0;
+            ay = 0;
         }
-        dp = new int[N+1][M+1];
-        ch = new int[N+1][M+1];
-        ax = (K/5) +1;
-        ay = K%5;
-        T.DFS(1,1);
-        System.out.println(cnt);
 
+        // DP 배열 초기화
+        dp1 = new int[N + 1][M + 1];
+        dp2 = new int[N + 1][M + 1];
+        dp1[1][1] = 1;
+        dp2[1][1] = 1;
 
-
+        if (K != 0) {
+            // 출발점에서 경유점까지의 경로 계산
+            T.dynamic1(ax, ay);
+            // 경유점에서 도착점까지의 경로 계산
+            T.dynamic2(N - ax + 1, M - ay + 1);
+            // 결과는 출발점->경유점 경로와 경유점->도착점 경로의 곱
+            int answer = dp1[ax][ay] * dp2[N - ax + 1][M - ay + 1];
+            System.out.println(answer);
+        } else {
+            // 경유점이 없을 경우, 출발점에서 도착점까지 직접 경로 계산
+            T.dynamic1(N, M);
+            System.out.println(dp1[N][M]);
+        }
     }
 }
