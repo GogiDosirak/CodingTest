76926b58e7287a0dee730a5da71c17b7fece1891
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-10-26 12:26:45 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 172 ms, Memory: 17508 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        switch(s) {
+            case "NLCS" :
+                System.out.println("North London Collegiate School");
+                break;
+            case "BHA" :
+                System.out.println("Branksome Hall Asia");
+                break;
+            case "KIS" :
+                System.out.println("Korea International School");
+                break;
+            case "SJA" :
+                System.out.println("St. Johnsbury Academy");
+                break;
+        }
+
+
+
+
+
+
+
+    }
+}
