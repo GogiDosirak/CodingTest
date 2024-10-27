3f861230c6d40168932a49ddb51c4287e84645ad
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-10-27 19:55:34 +0900

    [Bronze V] Title: 연세대학교, Time: 176 ms, Memory: 17428 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        
+        if(a == 0) {
+            System.out.println("YONSEI");
+        } else {
+            System.out.println("Leading the Way to the Future");
+        }
+
+
+
+
+
+
+
+    }
+}
