b828d6d21e6e8a8723c786c530909d32cd0d7936
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-10-29 19:20:33 +0900

    [Bronze V] Title: 홍익대학교, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Timestamp;
+import java.time.YearMonth;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       int N = Integer.parseInt(br.readLine());
+        System.out.println(N-1946);
+
+
+
+
+
+
+    }
+}
