00589835be6c9414ee4f6f6f6ccbf88bfe8160b4
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-10-31 14:49:59 +0900

    [Bronze IV] Title: R2, Time: 172 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.time.YearMonth;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+       Scanner sc = new Scanner(System.in);
+       int A = sc.nextInt();
+       int B = sc.nextInt();
+
+        System.out.println((B*2)-A);
+
+
+
+
+
+
+
+
+    }
+}
