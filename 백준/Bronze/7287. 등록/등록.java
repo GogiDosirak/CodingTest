9cfc1c20868a65a8cf276a5a65df6acc17c6e32d
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-11-03 19:25:43 +0900

    [Bronze V] Title: 등록, Time: 96 ms, Memory: 13856 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.time.YearMonth;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("265");
+        System.out.println("yyjjmm2003");
+
+
+
+
+
+
+
+
+
+
+    }
+}
