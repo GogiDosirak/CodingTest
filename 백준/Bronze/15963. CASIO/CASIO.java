43af030cf3feb20b771e4e27e8a9eab9448fe160
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-11-05 21:59:43 +0900

    [Bronze V] Title: CASIO, Time: 176 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.time.YearMonth;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+            if(a==b) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
