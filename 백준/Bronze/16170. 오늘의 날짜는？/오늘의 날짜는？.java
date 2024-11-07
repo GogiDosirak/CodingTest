5f7175acccb3fdab2c2336ebe52b7cd88561e176
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-11-08 02:44:07 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.time.*;
+import java.util.*;
+
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ZonedDateTime time = ZonedDateTime.now(ZoneId.of("UTC"));
+        System.out.println(time.getYear());
+        System.out.println(time.getMonthValue());
+        System.out.println(time.getDayOfMonth());
+
+
+
+
+
+
+
+
+
+
+    }
+}
