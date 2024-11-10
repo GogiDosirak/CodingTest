61f744742ffbe6c8a576153d4e480fa09fa6b1b9
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-11-10 17:22:24 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 180 ms, Memory: 18252 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.time.*;
+import java.util.*;
+
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::" + name + "::fan:");
+        System.out.println(":fan::fan::fan:");
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
