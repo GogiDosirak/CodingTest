bd44a2a3d79f4f92c6f4950bdde09d2f5ede0f53
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-11-14 15:10:41 +0900

    [Bronze IV] Title: 주사위, Time: 308 ms, Memory: 23128 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.time.*;
+import java.util.*;
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+       int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println("Case " + (i+1) + ": " + (a+b) );
+
+
+        }
+
+
+    }
+}
