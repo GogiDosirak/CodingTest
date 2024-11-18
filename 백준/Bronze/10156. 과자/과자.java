693933d44fd336e44d10228034ffa7682095e842
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-11-18 20:05:32 +0900

    [Bronze IV] Title: 과자, Time: 172 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.time.*;
+import java.util.*;
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+       int A = sc.nextInt();
+       int B = sc.nextInt();
+       int C = sc.nextInt();
+
+        
+       if(A*B < C) {
+           System.out.println(0);
+       } else {
+           System.out.println(Math.abs(C - (A * B)));
+       }
+
+
+    }
+}
