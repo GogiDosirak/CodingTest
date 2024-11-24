05bad3093cb05ad39b0dd35f1fc6afe1c94c9c5d
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-11-24 20:06:59 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 176 ms, Memory: 17452 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.time.*;
+import java.util.*;
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        
+        switch(s) {
+            case "SONGDO" :
+                System.out.println("HIGHSCHOOL");
+                break;
+            case "CODE" :
+                System.out.println("MASTER");
+                break;
+            case "2023" :
+                System.out.println("0611");
+                break;
+            case "ALGORITHM" :
+                System.out.println("CONTEST");
+                break;
+            
+            
+        }
+
+        
+
+
+    }
+}
