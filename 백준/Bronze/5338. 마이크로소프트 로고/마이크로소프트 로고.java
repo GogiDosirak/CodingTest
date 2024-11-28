42d761ceadbce1125ea16b5ce9f34ae49fbfaf14
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-11-28 16:22:02 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 96 ms, Memory: 13816 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.time.*;
+import java.util.*;
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-''-._|");
+
+
+
+
+
+
+    }
+}
