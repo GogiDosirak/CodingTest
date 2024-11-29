b53d53388917db70746ab1f8225574a19101b39f
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-11-29 18:03:26 +0900

    [Bronze IV] Title: 사장님 도박은 재미로 하셔야 합니다, Time: 228 ms, Memory: 19696 KB -BaekjoonHub

---
+++ 백준/Bronze/23795. 사장님 도박은 재미로 하셔야 합니다/사장님 도박은 재미로 하셔야 합니다.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.time.*;
+import java.util.*;
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        long sum = 0;
+
+        while(true) {
+            int a = sc.nextInt();
+            if(a==-1) {
+                break;
+            }
+            sum += a;
+
+
+
+
+        }
+
+        System.out.println(sum);
+
+
+
+
+
+
+    }
+}
