c435b6ef11afabc08a64ce3c9591a0dd57c1cfdf
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-11-30 12:30:41 +0900

    [Bronze V] Title: CAPS, Time: 400 ms, Memory: 27924 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.time.*;
+import java.util.*;
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        System.out.println(s.toUpperCase());
+
+
+
+
+
+    }
+}
