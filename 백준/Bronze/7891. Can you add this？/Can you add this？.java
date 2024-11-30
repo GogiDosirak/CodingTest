4fb12351fdd0b9465188a987396df07e9b5042f7
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-12-01 01:53:37 +0900

    [Bronze V] Title: Can you add this?, Time: 180 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.time.*;
+import java.util.*;
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        
+        Scanner sc = new Scanner(System.in);
+
+     
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a+b);
+            
+        }
+            
+            
+            
+            
+     
+
+
+
+
+
+    }
+}
