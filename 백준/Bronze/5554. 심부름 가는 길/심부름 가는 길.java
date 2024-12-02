0b8e4c796247aa386655ac310cbdb0878aae6f90
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-12-02 21:24:34 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 172 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.time.*;
+import java.util.*;
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        int sum = a+b+c+d;
+
+        System.out.println(sum/60);
+        System.out.println(sum%60);
+
+
+
+
+
+
+
+
+
+
+    }
+}
