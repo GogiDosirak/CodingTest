a87b6ed8c07ce60d9c4bfc6304537a8c21f87d64
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-12-05 14:13:05 +0900

    [Bronze III] Title: 첫 글자를 대문자로, Time: 100 ms, Memory: 14036 KB -BaekjoonHub

---
+++ 백준/Bronze/4458. 첫 글자를 대문자로/첫 글자를 대문자로.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.time.*;
+import java.util.*;
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+
+            System.out.println(String.valueOf(s.charAt(0)).toUpperCase() + s.substring(1,s.length()));
+
+
+        }
+    }
+}
