5fa36d452d5f6efee475beb187d8414801c88e47
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-12-18 23:09:38 +0900

    [Bronze V] Title: Serca, Time: 3576 ms, Memory: 69976 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.time.*;
+import java.util.*;
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(" @@@   @@@ ");
+            System.out.println("@   @ @   @");
+            System.out.println("@    @    @");
+            System.out.println("@         @");
+            System.out.println(" @       @ ");
+            System.out.println("  @     @  ");
+            System.out.println("   @   @   ");
+            System.out.println("    @ @    ");
+            System.out.println("     @     ");
+
+        }
+
+
+
+    }
+}
