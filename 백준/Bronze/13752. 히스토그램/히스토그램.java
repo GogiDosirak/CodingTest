36f6ca328f91645d8adf797500f705ded8c330a5
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-12-21 14:55:04 +0900

    [Bronze IV] Title: 히스토그램, Time: 260 ms, Memory: 18296 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.time.*;
+import java.util.*;
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            int a = sc.nextInt();
+            for (int j = 0; j < a; j++) {
+                System.out.print("=");
+            }
+            System.out.println();
+
+        }
+
+    }
+}
