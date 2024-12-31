de24c3aadfaf0ed158e4143bb860280414f52e7c
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-12-31 22:37:46 +0900

    [Bronze III] Title: 이 문제는 D2 입니다., Time: 212 ms, Memory: 17888 KB -BaekjoonHub

---
+++ 백준/Bronze/23303. 이 문제는 D2 입니다．/이 문제는 D2 입니다．.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.time.*;
+import java.util.*;
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        
+        String s = sc.nextLine();
+        
+        if(s.contains("d2") || s.contains("D2")) {
+            System.out.println("D2");
+            
+        } else {
+            System.out.println("unrated");
+        }
+
+
+
+
+    }
+}
