29dacc3502b5c3eaf95de1a240a74802e245e97a
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-07-24 10:58:20 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 59.09 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int n : arr) {
+            if(stack.size() == 0 || stack.peek() != n) {
+               stack.push(n); 
+            }
+        }
+        
+        int[] answer = new int[stack.size()];
+        
+        for(int i = stack.size() - 1; i >= 0; i--) {
+            answer[i] = stack.pop();
+        }
+        
+
+        return answer;
+    }
+}
