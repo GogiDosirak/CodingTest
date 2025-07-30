5d153d4566bea14f39468ae537e76ba1a290f845
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-07-30 14:24:36 +0900

    [level 2] Title: 배달, Time: 3.99 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -2,51 +2,47 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int[][] road, int K) {
-        int[] dis = new int[N+1];
         List<ArrayList<Edge>> graph = new ArrayList<>();
         
-        for(int i = 0; i < N+1; i++) {
+        for(int i = 0; i <= N; i++) {
             graph.add(new ArrayList<>());
         }
         
-        for(int i = 0 ; i < road.length; i++) {
+        for(int i = 0; i < road.length; i++) {
             graph.get(road[i][0]).add(new Edge(road[i][1], road[i][2]));
             graph.get(road[i][1]).add(new Edge(road[i][0], road[i][2]));
         }
-        
+        int[] dis = new int[N+1];
         Arrays.fill(dis, Integer.MAX_VALUE);
+        
         dis[1] = 0;
-        PriorityQueue<Edge> queue = new PriorityQueue<>((a,b) -> a.cost - b.cost);
-        queue.add(new Edge(1,0));
         
+        PriorityQueue<Edge> pq = new PriorityQueue<>((a,b) -> a.cost - b.cost);
+        pq.add(new Edge(1,0));
         
-        while(!queue.isEmpty()) {
-            Edge curr = queue.poll();
+        while(!pq.isEmpty()) {
+            Edge curr = pq.poll();
             int vex = curr.vex;
             int cost = curr.cost;
             
-            if(cost > dis[vex]) {
-                continue;
-            }
+            if(cost > dis[vex]) continue;
             
             for(Edge next : graph.get(vex)) {
-                int newDist = dis[vex] + next.cost;
-                if(dis[next.vex] > newDist) {
-                    dis[next.vex] = newDist;
-                    queue.add(new Edge(next.vex, newDist));
+                if(next.cost + dis[vex] < dis[next.vex]) {
+                    dis[next.vex] = next.cost + dis[vex];
+                    pq.add(new Edge(next.vex, dis[next.vex]));
                 }
             }
         }
-            
-            int answer = 0;
-            
-            for(int n : dis) {
-                if(K >= n) {
-                    answer++;
-                }
+        
+        int answer = 0;
+        for(int n : dis) {
+            if(n <= K) {
+                answer++;
             }
-            
+        }
         return answer;
+        
     }
 }
 
