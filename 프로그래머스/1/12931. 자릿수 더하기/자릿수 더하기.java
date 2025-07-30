a748894f51851eec1c3023c97f47b17dfae59329
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-07-30 21:07:48 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        int curr = n;
+        while(curr > 0) {
+            answer += curr%10;
+            curr = curr/10;
+        }
+
+
+        return answer;
+    }
+}
