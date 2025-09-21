3a9cc702f7ae7e67547cddcc68ee9125c84b416e
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-09-21 16:35:42 +0900

    [level 3] Title: 네트워크, Time: 0.73 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,48 +1,43 @@
+import java.io.*;
 import java.util.*;
 
 class Solution {
-    static int[] unf;
+    boolean[] visited;
     public int solution(int n, int[][] computers) {
-        unf = new int[computers.length];
+        // 네트워크의 개수 판별 -> grid에서 같은 빙산?의 영역 구하는 것과 같다. 그것의 그래프 버전
+        // 1이면 탐색 시작 count ++ -> 방문했다면 0으로 바꿔주고.. 1이면 또 탐색 시작하고 count++
+        int answer = 0;
         
-        for(int i = 0; i < unf.length; i++) {
-            unf[i] = i;
-        }
-        
-        for(int i = 0 ; i < computers.length; i++) {
-            for(int j = 0; j < computers[0].length; j++) {
-                if(i != j && computers[i][j] == 1) {
-                    union(i,j);
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i = 0; i < computers.length; i++) {
+            list.add(new ArrayList<>());
+            for(int j = 0; j < computers[i].length; j++) {
+                // 연결이 있을 때만 1
+                if(computers[i][j] == 1 && i != j) {
+                    list.get(i).add(j);
                 }
             }
         }
         
-        Set<Integer> set = new HashSet<>();
+        visited = new boolean[computers.length];
         
-        for(int i = 0; i < unf.length; i++) {
-            set.add(find(i));
+        for(int i = 0; i < computers.length; i++) {
+            // 해당 노드를 탐색하지 않았다면
+            if(visited[i] == false) {
+                visited[i] = true;
+                answer++;
+                DFS(list, i);
+            }
         }
-        
-        return set.size();
-        
-      
+        return answer;
     }
     
-    public static void union(int a, int b) {
-        int fa = find(a);
-        int fb = find(b);
-        if(fa != fb) {
-            unf[fa] = fb;
+    public void DFS(List<List<Integer>> list, int node) {
+        for(int n : list.get(node)) {
+            if(visited[n] == false) {
+                visited[n] = true;
+                DFS(list, n);
+            }
         }
     }
-    
-    public static int find(int v) {
-        if(v == unf[v]) {
-            return v;
-        } else {
-            return unf[v] = find(unf[v]);
-        }
-    }
-    
- 
 }
